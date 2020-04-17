abdf02712418952024b529288c1c31632bc57499
Wasurat <Aumaumone2009@gmail.com>
2020-04-17 23:54:27 +0700

    49.00

--- polls/admin.py
+++ polls/admin.py
@@ -5,11 +5,22 @@ from .models import Question , Choice
 # admin.site.register(Question)
 # admin.site.register(Choice)
 
+admin.site.site_header = "Pollster Admin"
+admin.site.site_title = "Pollster Admin Area"
+admin.site.index_tile = "Welcome to The Pollster Admin"
+
 class ChoiceInline(admin.TabularInline):
     model = Choice
     extra = 3
 
 class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None , {'fields' : ['question_text']}),
+        ('Date Information',{'fields' : ['pub_date'] ,'classes':['collapse']},)
+
+    ]
     
-    
+    inlines = [ChoiceInline]   
+
+admin.site.register(Question , QuestionAdmin)    
 
